12f6078014f8c9355b9f7c0d36292db986f8ec3a
dammatsu.yuta <dammatsu.yuta@moneyforward.co.jp>
2022-07-17 08:43:07 +0900

    add css libraries

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import styled from 'styled-components';
 import styles from '../styles/Home.module.css'
+
+const HeaderWrapper = styled.div`
+  grid-area: header;
+  text-align: center;
+`;
 
 const Home: NextPage = () => {
   return (
