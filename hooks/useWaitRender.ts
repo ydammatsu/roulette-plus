88cb64d4539109d7a8a4ceec428d9188230c4e74
dammatsu.yuta <dammatsu.yuta@moneyforward.co.jp>
2022-07-20 14:47:38 +0900

    comment を使う

--- hooks/useWaitRender.ts
+++ hooks/useWaitRender.ts
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 
+// 初回描画時に styled components に style が当たらない瞬間が存在するので render をブロックするために使う
 export const useWaitRender = () => {
   const [isRendered, setIsRendered] = useState(false)
   useEffect(() => {
