28548c55ef45c32aa14a7c54c39989510c27b00f
dammatsu.yuta <dammatsu.yuta@moneyforward.co.jp>
2022-07-20 14:49:31 +0900

    refactoring

--- hooks/useRoulette.ts
+++ hooks/useRoulette.ts
@@ -6,13 +6,11 @@ import { useQuery } from "urql";
 export const useRoulette = (name: string, pause: boolean) => {
   const [currentWinner, setCurrentWinner] = useState<Candidate>({ idx: -1, name: "dummy", hide: true })
   const [roulette, setRoulette] = useState<Roulette>({id: "dummy", name: "dummy", candidates: []});
-  
-  const [result, _] = useQuery({
+  const [{ data, fetching, error }, _] = useQuery({
     query: GetRouletteQuery,
     variables: { name },
     pause: pause
   });
-  const { data, fetching, error } = result;
 
   useEffect(() => {
     if (!fetching) {
@@ -21,11 +19,11 @@ export const useRoulette = (name: string, pause: boolean) => {
         const id = rouleteData.id
         const name = rouleteData.name
         const candidates = rouleteData.candidates.map((value: string) => {return JSON.parse(value)})
-        
+
         setRoulette({ id, name, candidates })
       }
     }
-  }, [data, fetching, result])
+  }, [data, fetching])
 
   return({roulette, setRoulette, currentWinner, setCurrentWinner})
 }
